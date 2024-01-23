ffb4c29f54aaa1c3bf798bac847c056eea8efdc8
aymaneaarab <aymanaarab5122003@gmail.com>
2024-01-23 00:47:21 +0000

    logo of bookmarks change his color when its clicked

--- src/Components/ArRecipeLayout.jsx
+++ src/Components/ArRecipeLayout.jsx
@@ -1,39 +1,50 @@
 import { useState } from "react";
 // import "./App.css";
-import { CiBookmark } from "react-icons/ci"; //bookmark vide 
+import { CiBookmark } from "react-icons/ci"; //bookmark vide
 import { FaBookmark } from "react-icons/fa"; // bookmark with color
 
 import { useEffect } from "react";
+import { Alert } from "@mui/material";
 const key = "cbed10cacb9c81e2a7e48b59678ca090	";
 const app_id = "b90b16e8";
 
 export default function ArRecipe() {
-useEffect(()=>{
-  setTimeout(() => {
-    
-  }, 5000);
-},[])
-
-
-
+  useEffect(() => {
+    setTimeout(() => {}, 5000);
+  }, []);
+const [bookmarkclicked,setbookmarkclicked]=useState(false);
   const [query, setquery] = useState("");
   const [recipes, setrecipes] = useState();
-  let searchedRecipes = recipes?.filter((r)=>r?.recipe.title.includes(query) );
-  if (query===""){
-    searchedRecipes =[]
+  let searchedRecipes = recipes?.filter((r) => r?.recipe.title.includes(query));
+  if (query === "") {
+    searchedRecipes = [];
   }
 
-
+ 
   const [selectedId, setselectedId] = useState("");
   function selectrecipe(id) {
     setselectedId(id);
   }
 
+  //bookmarks states and functions 👍
+
+  const [bookmark, setbookmark] = useState([]);
+
+  function addtobookmark(newb) {
+    setbookmark([...bookmark, newb]);
+  }
+  function clickbookmark(){
+    setbookmarkclicked(!bookmarkclicked)
+  }
+
   return (
     <div className="bg-green-100 h-screen">
-      <Header query={query} setquery={setquery} />
+      <Header query={query} setquery={setquery} clickbookmark={clickbookmark} bookmarkclicked={bookmarkclicked} />
       <div className="grid grid-cols-3 h-96 gap-9 my-8">
-        <RecipeBoard selectedrecipe={selectedId} />
+        <RecipeBoard
+          selectedrecipe={selectedId}
+          addtobookmark={addtobookmark}
+        />
         <div className="h-auto w-1 bg-slate-400 justify-self-center"></div>
         <RecipeList
           query={query}
@@ -46,13 +57,15 @@ useEffect(()=>{
   );
 }
 
-function Header({ query, setquery }) {
+function Header({ query, setquery , clickbookmark , bookmarkclicked }) {
   return (
     <div className="bg-green-200	flex justify-between items-center p-5 rounded text-white content-center   ">
-      <div className="flex">
+      <div className="flex" onClick={clickbookmark}>
+      {
+      !bookmarkclicked ?  <CiBookmark className="bookmark" /> : <FaBookmark className="bookmark"/>
 
-      <CiBookmark className="bookmark" />
-       <pre>الوصفات المحفوظة</pre>
+      } 
+        <pre>الوصفات المحفوظة</pre>
       </div>
       <div className="">
         <input
@@ -66,19 +79,21 @@ function Header({ query, setquery }) {
       <div className="flex">
         <span className="text-xl mx-3">🥦</span>
         <p className="font-semibold tracking-widest font-mono"> Recipe</p>
-      </div>    </div>
+      </div>{" "}
+    </div>
   );
 }
 function RecipeList({ query, recipes, setrecipes, selectrecipe }) {
   useEffect(() => {
     const controller = new AbortController();
-   const timefetching = setTimeout(()=>{RecipeFetch()}, 5000);
+    const timefetching = setTimeout(() => {
+      RecipeFetch();
+    }, 5000);
     async function RecipeFetch() {
       try {
-        const res = await fetch(
-         "http://localhost:9000/recipes"   ,
-          { signal: controller.signal }
-        );
+        const res = await fetch("http://localhost:9000/recipes", {
+          signal: controller.signal,
+        });
         if (!res.ok) {
           throw new Error("something wrong ");
         }
@@ -95,7 +110,7 @@ function RecipeList({ query, recipes, setrecipes, selectrecipe }) {
         console.log(err);
       }
     }
-    if (!query.length || query.length<1 || query==="") {
+    if (!query.length || query.length < 1 || query === "") {
       setrecipes([]);
       return;
     }
@@ -104,7 +119,7 @@ function RecipeList({ query, recipes, setrecipes, selectrecipe }) {
       clearTimeout(timefetching);
       controller.abort();
     }; // component unmount
-  }, [query,setrecipes]);
+  }, [query, setrecipes]);
 
   return (
     <div className=" bg-green-300 rounded-xl h-96 text-white overflow-auto  ml-16 p-2 ">
@@ -133,37 +148,60 @@ function RecipeList({ query, recipes, setrecipes, selectrecipe }) {
   );
 }
 
-function RecipeBoard({ selectedrecipe }) {
-  console.log(selectedrecipe)
+function RecipeBoard({ selectedrecipe, addtobookmark }) {
+  // console.log(selectedrecipe)
   const [data, setdata] = useState([]);
-  console.log("data",data)
-const filtredata = Array.isArray(data) && data.filter((d)=>d?.recipe?.recipe_id===selectedrecipe ) ;
-console.log("test",filtredata) ;
-useEffect(()=>{
-async function recipeselectedfetch(){
-const res= await fetch("http://localhost:9000/recipes").then((res)=>res.json()).then((res)=>setdata(res.recipes))
-}
-recipeselectedfetch();
-},[selectedrecipe])
+  // console.log("data",data)
+  const filtredata =
+    Array.isArray(data) &&
+    data.filter((d) => d?.recipe?.recipe_id === selectedrecipe);
+  // console.log("test",filtredata) ;
+  // const [added, setaded] = useState(false);
+  useEffect(() => {
+    async function recipeselectedfetch() {
+      const res = await fetch("http://localhost:9000/recipes")
+        .then((res) => res.json())
+        .then((res) => setdata(res.recipes));
+    }
+    recipeselectedfetch();
+  }, [selectedrecipe]);
+
+  const newbookmark = {
+    title: filtredata[0]?.recipe?.title,
+    image_url: filtredata[0]?.recipe?.image_url,
+  };
+  function recipeboardaddbtn(e) {
+    e.preventDefault();
+    addtobookmark(newbookmark);
+    // setaded(!added);
+   
+
+  }
+
+  
   return (
     <div className=" bg-green-300 rounded-xl h-96 text-white overflow-scroll overflow-x-hidden mr-9 p-2">
       <div>
-        
-        {selectedrecipe && filtredata && 
-      <div>
-<h1>{filtredata[0]?.recipe?.title}</h1>
-      <img src={filtredata[0]?.recipe?.image_url} alt="" />
-      <span>
-        <h6>:المكونات</h6>
-        <ul>
-        {filtredata[0]?.recipe?.ingredients.map(ing=><li>{ing}</li>)}
-        </ul>
-      </span>
-      <button className="bg-green-500 p-4">اضافة للمفضلة</button>
-      </div>
-      
-      }
-      
+        {selectedrecipe && filtredata && (
+          <div>
+            <h1>{filtredata[0]?.recipe?.title}</h1>
+            <img src={filtredata[0]?.recipe?.image_url} alt="" />
+            <span>
+              <h6>:المكونات</h6>
+              <ul>
+                {filtredata[0]?.recipe?.ingredients.map((ing) => (
+                  <li>{ing}</li>
+                ))}
+              </ul>
+            </span>
+            <button
+              className="bg-green-500 p-4"
+              onClick={(e) => recipeboardaddbtn(e)}
+            >
+              اضافة للمفضلة
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
