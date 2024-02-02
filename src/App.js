42f4b2623d08518dbfe72297ddda3a6bf4411fe0
aymaneaarab <aymanaarab5122003@gmail.com>
2024-02-02 17:40:22 +0000

    organisation

--- src/App.js
+++ src/App.js
@@ -2,16 +2,15 @@ import React from 'react';
 import './App.css';
 import Homepage from './Test/HomePage';
 import LayoutRecipe from './Components/LayoutRecipe';
-import ArRecipe from './Components/ArRecipeLayout';
 import { Route, Routes } from 'react-router';
+import ArRecipe from './Components/ArRecipeLayout/ArRecipeLayout';
 
 
 
 function App() {
   return (
 
-<ArRecipe/>
-)
+<ArRecipe/>)
 
 }
 
