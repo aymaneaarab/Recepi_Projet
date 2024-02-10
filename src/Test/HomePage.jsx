442b7079ee527d2a8f7c64aacb07ab7af5e78920
Kharbachzoubair <zoubairkharbach@gmail.com>
2024-02-10 17:44:35 +0100

    arabic home page

--- src/Test/HomePage.jsx
+++ src/Test/HomePage.jsx
@@ -3,65 +3,84 @@ import styles from "./HomePage.module.css";
 import { NavLink } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { change_lang } from "../redux/LanguaSlicer";
-// import recepi from "./recepi.png";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+
 export default function Homepage() {
-  useEffect(()=>{
+  useEffect(() => {
     AOS.init();
-  },[])
+  }, []);
+
+  const dispatch = useDispatch();
+  const langue = useSelector((state) => state.Langue.choice);
+
+  function handleLangChange(e) {
+    dispatch(change_lang(e.target.value));
+  }
+
   return (
-   
     <main className={styles.homepage}>
-      <PageNav />
+      <PageNav handleLangChange={handleLangChange} langue={langue} />
 
       <section>
-      <div data-aos="zoom-in"  data-aos-duration="2000">
-        <h1>
-          Find your prefered recipets just by one click.
-          <br />
-          With Recepi
-        </h1>
-        <h2>
-          <pre>
-
-          A site that provide to you more than 1.000.000 recipites and give the
-             disponibility of adding recipites to your bookmark ☘
-          </pre>
-        </h2>
-        {/* <a href="google.com" className={styles.cta}>
-          Start Now !!
-        </a> */}
-        <NavLink to="search" className={styles.cta}>
-          {" "}
-          Start Now !!{" "}
-        </NavLink>
-    </div>
+        <div data-aos="zoom-in" data-aos-duration="2000">
+          <h1>
+            {langue === "ar" ? (
+              <>
+                <span>اعثر على وصفاتك المفضلة بنقرة واحدة.</span>
+                <br />
+                <span>مع ريسيبي</span>
+              </>
+            ) : (
+              <>
+                <span>Find your preferred recipes just by one click.</span>
+                <br />
+                <span>With Recepi</span>
+              </>
+            )}
+          </h1>
+          <h2>
+            {langue === "ar" ? (
+              <>
+                <span>موقع يوفر لك أكثر من 1.000.000 وصفة ويوفر لك</span>
+                <br />
+                <span>إمكانية إضافة الوصفات إلى قائمة المفضلة ☘</span>
+              </>
+            ) : (
+              <>
+                <span>
+                  A site that provides you with more than 1,000,000 recipes and
+                  gives you the ability to add recipes to your bookmarks ☘
+                </span>
+              </>
+            )}
+          </h2>
+          <NavLink to="search" className={styles.cta}>
+            {langue === "ar" ? "ابدأ الآن !!" : "Start Now !!"}
+          </NavLink>
+        </div>
       </section>
     </main>
   );
 }
 
-function PageNav() {
-  const dispatch = useDispatch();
-  const langue = useSelector((state) => state.Langue.choice);
-  function handlechange(e) {
-    dispatch(change_lang(e.target.value));
-  }
+function PageNav({ handleLangChange, langue }) {
   return (
     <nav className={styles.nav}>
-      <Logo />
-
+      <Logo langue={langue} />
       <ul>
         <li>
           <a href="google.com">About us</a>
         </li>
         <li>
-          <a href="#">Choose your language</a>{" "}
-          <select name="" id="" value={langue} onChange={handlechange}>
-            {/* <option value="" disabled>Choose your Language </option> */}
+          <span>
+            {langue === "ar"
+              ? "اختر لغتك"
+              : "Choose your language"}{" "}
+          </span>
+          <select name="" id="" value={langue} onChange={handleLangChange}>
             <option value="eng">English</option>
-            <option value="ar">Arabic</option>
+            <option value="ar">العربية</option>
           </select>{" "}
         </li>
       </ul>
@@ -69,12 +88,11 @@ function PageNav() {
   );
 }
 
-function Logo() {
+function Logo({ langue }) {
   return (
     <a href="google.com">
-      {/* <img src={recepi} alt="recepi logo" className={styles.logo} /> */}
       <h3 className="text-3xl font-semibold tracking-widest font-mono">
-        🥦 Recepi
+        🥦 {langue === "ar" ? "ريسيبي" : "Recepi"}
       </h3>
     </a>
   );
