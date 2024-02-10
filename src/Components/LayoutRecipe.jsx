081688d0abccc176f251cac03722e649174de243
Kharbachzoubair <zoubairkharbach@gmail.com>
2024-02-10 17:04:14 +0100

    i matched the english version with the arabic one design's and added some messages to the bookmark when the recipe empty and you click on the saved recipe "Your bookmark list is empty !!!!!!!" will appear

--- src/Components/LayoutRecipe.jsx
+++ src/Components/LayoutRecipe.jsx
@@ -4,6 +4,9 @@ import { FaBookmark } from "react-icons/fa";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { NavLink } from "react-router-dom";
+import { AiOutlineCloseCircle } from "react-icons/ai";
+import { Popconfirm } from 'antd';
+import { QuestionCircleOutlined } from '@ant-design/icons';
 
 const key = "cbed10cacb9c81e2a7e48b59678ca090";
 const app_id = "b90b16e8";
@@ -27,10 +30,9 @@ export default function LayoutRecipe() {
     if (!isRecipeInBookmarks) {
       setBookmarks((prevBookmarks) => [...prevBookmarks, recipe]);
 
-      // Display a success message using toast
       toast.success(`Recipe "${recipe.label}" added to bookmarks!`, {
         position: "top-right",
-        autoClose: 3000, // 3 seconds
+        autoClose: 3000,
         hideProgressBar: false,
         closeOnClick: true,
         pauseOnHover: true,
@@ -38,7 +40,6 @@ export default function LayoutRecipe() {
         progress: undefined,
       });
     } else {
-      // Display a message indicating the recipe is already in bookmarks
       toast.warn(`Recipe "${recipe.label}" is already in bookmarks.`, {
         position: "top-right",
         autoClose: 3000,
@@ -96,10 +97,19 @@ export default function LayoutRecipe() {
 
   return (
     <div className="bg-gray-100 h-screen">
-      <Header query={query} setQuery={setQuery} handleViewBookmarksClick={handleViewBookmarksClick} />
+      <Header
+        query={query}
+        setQuery={setQuery}
+        handleViewBookmarksClick={handleViewBookmarksClick}
+      />
 
       <div className="grid grid-cols-3 gap-4 h-4/5 mx-auto mt-8 p-4">
-        <RecipeList query={query} recipes={recipes} setRecipes={setRecipes} selectRecipe={selectRecipe} />
+        <RecipeList
+          query={query}
+          recipes={recipes}
+          setRecipes={setRecipes}
+          selectRecipe={selectRecipe}
+        />
 
         <div className="h-full w-1 bg-gray-400 justify-self-center"></div>
 
@@ -108,7 +118,10 @@ export default function LayoutRecipe() {
             <BookmarkList bookmarks={bookmarks} setBookmarks={setBookmarks} />
           </div>
         ) : (
-          <RecipeBoard selectedRecipeId={selectedId} addToBookmarkList={addToBookmarkList} />
+          <RecipeBoard
+            selectedRecipeId={selectedId}
+            addToBookmarkList={addToBookmarkList}
+          />
         )}
       </div>
 
@@ -122,11 +135,11 @@ function Header({ query, setQuery, handleViewBookmarksClick }) {
 
   const clickbookmark = () => {
     setBookmarkClicked(!bookmarkClicked);
-    handleViewBookmarksClick(); // This will toggle the showBookmarkList state
+    handleViewBookmarksClick();
   };
 
   return (
-    <div className="bg-green-600 flex justify-between items-center p-8 rounded text-gray-200"> {/* Increased padding */}
+    <div className="bg-green-600 flex justify-between items-center p-8 rounded text-gray-200">
       <NavLink to="/">
         <div className="flex">
           <span className="text-xl mx-3">🥦</span>
@@ -136,7 +149,7 @@ function Header({ query, setQuery, handleViewBookmarksClick }) {
       <div className="">
         <input
           type="text"
-          className="bg-green-300 placeholder-white rounded p-2 w-40" // Decreased width
+          className="bg-green-300 placeholder-white rounded p-2 w-40"
           placeholder="Search for a recipe"
           value={query}
           onChange={(e) => setQuery(e.target.value)}
@@ -213,8 +226,8 @@ function RecipeList({ query, recipes, setRecipes, selectRecipe }) {
             className="rounded"
           />
 
-          <div className="text-green-600">
-            <h3 className="text-lg">{recipe.recipe.label}</h3>
+          <div className="text-green-600 p-4">
+            <h3 className="text-lg font-extrabold">{recipe.recipe.label}</h3>
             <span>Author: {recipe.recipe.source}</span>
           </div>
         </div>
@@ -224,72 +237,87 @@ function RecipeList({ query, recipes, setRecipes, selectRecipe }) {
 }
 
 function RecipeBoard({ selectedRecipeId, addToBookmarkList }) {
-  const [data, setData] = useState();
+  const [data, setData] = useState(null);
 
   useEffect(() => {
-    const controller = new AbortController();
+    if (selectedRecipeId) {
+      const controller = new AbortController();
 
-    async function fetchRecipeDetails() {
-      try {
-        const res = await fetch(selectedRecipeId, { signal: controller.signal });
-        if (!res.ok) {
-          throw new Error("Failed to fetch recipe details");
+      async function fetchRecipeDetails() {
+        try {
+          const res = await fetch(selectedRecipeId, { signal: controller.signal });
+          if (!res.ok) {
+            throw new Error("Failed to fetch recipe details");
+          }
+
+          const data = await res.json();
+          if (data.response === false) {
+            throw new Error("Recipe details not found");
+          }
+
+          setData(data?.recipe);
+        } catch (err) {
+          console.log(err);
         }
-
-        const data = await res.json();
-        if (data.response === false) {
-          throw new Error("Recipe details not found");
-        }
-
-        setData(data?.recipe);
-      } catch (err) {
-        console.log(err);
       }
+
+      fetchRecipeDetails();
+
+      return function () {
+        controller.abort();
+      };
     }
-
-    fetchRecipeDetails();
-
-    return function () {
-      controller.abort();
-    };
   }, [selectedRecipeId]);
 
   const handleAddToBookmarkClick = () => {
     addToBookmarkList(data);
   };
 
+  const handleClearRecipe = () => {
+    setData(null);
+  };
+
   return (
-    <div className="bg-gray-100 rounded-xl text-green-600 overflow-scroll p-4 border border-gray-400">
+    <div className={`bg-gray-100 rounded-xl text-green-600 overflow-scroll p-4 border border-gray-400 relative`}>
       {data && (
         <div>
           <img src={data?.image} alt={data?.label} className="rounded w-full" />
 
           <div className="mt-2">
-            <h2 className="text-xl">{data?.label}</h2>
+            <h2 className="text-xl text-center text-white border bg-green-500 p-2">{data?.label}</h2>
             <p>{data?.description}</p>
           </div>
 
           <div className="mt-4">
-            <h3 className="text-lg">Ingredients:</h3>
-            <ul>
+            <h3 className="text-xl text-black text-center mt-2 border border-b underline p-1">Ingredients:</h3>
+            <ul className="text-left text-xl font-extrabold">
               {data?.ingredients.map((ingredient, index) => (
                 <li key={index}>{ingredient.text}</li>
               ))}
             </ul>
           </div>
+          <center>
+            <button
+              className="bg-green-500 p-4 text-white text-center"
+              onClick={handleAddToBookmarkClick}
+            >
+              Add to Bookmarks
+            </button>
+          </center>
+          
 
+          {/* Clear button */}
           <button
-            className="bg-green-600 text-gray-200 px-4 py-2 rounded mt-4 hover:bg-gray-200"
-            onClick={handleAddToBookmarkClick}
+            className="absolute top-4 right-4 bg-green-600 text-white p-2 rounded-full hover:bg-gray-200"
+            onClick={handleClearRecipe}
           >
-            Ajouter aux Favoris
+            <AiOutlineCloseCircle size={20} />
           </button>
         </div>
       )}
     </div>
   );
 }
-
 function BookmarkList({ bookmarks, setBookmarks }) {
   const handleRemoveClick = (index) => {
     const updatedBookmarks = [...bookmarks];
@@ -298,39 +326,52 @@ function BookmarkList({ bookmarks, setBookmarks }) {
   };
 
   return (
-    <div className="bg-gray-100 rounded-xl text-green-600 p-4 border border-gray-400">
+    <div className="bg-gray-100 rounded-xl text-green-600 overflow-auto p-4 border border-gray-400">
+      {bookmarks.length === 0 && <h3 className="text-lg font-semibold mb-4  text-center">Your bookmark list is empty !!!!!!!</h3>}
+      {bookmarks.length > 0 && <h3 className="text-lg font-semibold mb-4  text-center">Here are your saved recipes:</h3>}
       {bookmarks.map((recipe, i) => (
-        <div key={i} className="flex border border-b-1 bg-transparent gap-2 p-4">
-          <img
-            src={recipe.image}
-            alt={recipe.label}
-            height="60px"
-            width="70px"
-            className="rounded"
-          />
+        <div key={i} className="flex border border-b-1 bg-transparent gap-3 p-9 justify-between">
+          <div className="flex">
+            <img
+              src={recipe.image}
+              alt={recipe.label}
+              height="90px"
+              width="90px"
+              className="rounded"
+            />
 
-          <div className="text-green-600">
-            <h3 className="text-lg">{recipe.label}</h3>
-            <span>Author: {recipe.source}</span>
+            <div className="flex flex-col">
+              <div className="flex text-green-600 font-extrabold text-xl px-5 py-8">
+                <h3 className="text-xl ">{recipe.label}</h3>
+              </div>
+
+            </div>
           </div>
 
           <div className="flex items-center">
-            <button
-              className="bg-red-500 text-gray-200 px-3 py-1 rounded mr-2 hover:bg-gray-200"
-              onClick={() => handleRemoveClick(i)}
+            <Popconfirm
+              title="Delete Recipe"
+              description="Are you sure you want to delete the recipe?"
+              cancelText="Cancel"
+              okText="Delete"
+              onConfirm={() => handleRemoveClick(i)}
+              okType="danger"
+              icon={<QuestionCircleOutlined style={{ color: "red" }} />} // Assuming QuestionCircleOutlined is defined
             >
-              Retirer
-            </button>
-            {/* Replace 'url' with the actual property containing the recipe URL */}
-            <a
-              href={recipe.url}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <button className="bg-blue-500 text-gray-200 px-3 py-1 rounded hover:bg-gray-200">
-                Voir
+              <button className="bg-red-500 text-gray-200 px-3 py-1  rounded hover:bg-gray-200">
+                remove
               </button>
-            </a>
+            </Popconfirm>
+            <div>
+                <a
+                  href={recipe.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="bg-blue-500 text-gray-200 px-3 py-1 rounded hover:bg-gray-200 inline-block"
+                >
+                  view
+                </a>
+              </div>            
           </div>
         </div>
       ))}
